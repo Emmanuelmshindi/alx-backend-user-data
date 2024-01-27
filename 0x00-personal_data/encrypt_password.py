aa2165ffc7abec18e1ce2747915621e445905097
Emmanuelmshindi <mshindimanuel@gmail.com>
2024-01-27 22:56:15 +0000

    Revise task 6

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -3,12 +3,12 @@
 import bcrypt
 
 
-def hash_password(password: str) -> byte:
+def hash_password(password: str) -> bytes:
     """Returns a salted, hashed password"""
     return bcrypt.hashpw(password.encode('utf-8'), bcypt.gensalt())
 
 
-def is_valid(hashed_password: bytes, password: str):
+def is_valid(hashed_password: bytes, password: str) -> bool:
     """Check if password is valid in relation to
     the hashed password"""
     return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
